520df44343b18390fc5bde8138a27a3b09e37c4f
AnOwlBe <iamaowlbe@gmail.com>
2026-06-04 16:47:05 -0400

    Changes for CondIsKeyedBossBar.java & ExprKeyOfBossBar.java

--- src/main/java/org/skriptlang/skript/bukkit/bossbar/elements/expressions/ExprKeyOfBossBar.java
+++ src/main/java/org/skriptlang/skript/bukkit/bossbar/elements/expressions/ExprKeyOfBossBar.java
@@ -4,20 +4,12 @@ import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Example;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
-import ch.njol.skript.lang.SyntaxStringBuilder;
+import ch.njol.skript.expressions.base.SimplePropertyExpression;
 import org.bukkit.boss.BossBar;
 import org.bukkit.boss.KeyedBossBar;
-import ch.njol.skript.lang.Expression;
-import ch.njol.skript.lang.SkriptParser.ParseResult;
-import ch.njol.skript.lang.util.SimpleExpression;
-import ch.njol.util.Kleenean;
-import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
-import org.skriptlang.skript.registration.SyntaxInfo;
 import org.skriptlang.skript.registration.SyntaxRegistry;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Name("Key Of Boss Bar")
 @Description("""
@@ -28,41 +20,29 @@ import java.util.List;
 	broadcast the boss bar key of {_mybar}
 	""")
 @Since("INSERT VERSION")
-public class ExprKeyOfBossBar extends SimpleExpression<String> {
+public class ExprKeyOfBossBar extends SimplePropertyExpression<BossBar, String> {
 
 	public static void register(SyntaxRegistry registry) {
 		registry.register(
 			SyntaxRegistry.EXPRESSION,
-			SyntaxInfo.Expression.builder(ExprKeyOfBossBar.class, String.class)
-				.addPatterns("[the] boss[ ]bar (key|id) of %bossbars%")
+			infoBuilder(
+				ExprKeyOfBossBar.class,
+				String.class,
+				"boss[ ]bar (key|id)",
+				"bossbars",
+				false
+			)
 				.supplier(ExprKeyOfBossBar::new)
 				.build()
 		);
 	}
 
-	private Expression<BossBar> barExpr;
-
 	@Override
-	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
-		barExpr = (Expression<BossBar>) exprs[0];
-		return true;
-	}
-
-	@Override
-	protected String @Nullable [] get(Event event) {
-		List<String> keys = new ArrayList<>();
-		for (BossBar bar : barExpr.getArray(event)) {
-			if (bar instanceof KeyedBossBar keyed) {
-				keys.add(keyed.getKey().toString());
-			}
+	public @Nullable String convert(BossBar bar) {
+		if (bar instanceof KeyedBossBar keyed) {
+			return keyed.getKey().toString();
 		}
-		return keys.toArray(new String[0]);
-	}
-
-
-	@Override
-	public boolean isSingle() {
-		return false;
+		return null;
 	}
 
 	@Override
@@ -71,11 +51,8 @@ public class ExprKeyOfBossBar extends SimpleExpression<String> {
 	}
 
 	@Override
-	public String toString(@Nullable Event event, boolean debug) {
-		SyntaxStringBuilder builder = new SyntaxStringBuilder(event, debug);
-		builder.append("the keys of");
-		builder.append(barExpr);
-		return builder.toString();
+	protected String getPropertyName() {
+		return "boss bar key";
 	}
 
 }
