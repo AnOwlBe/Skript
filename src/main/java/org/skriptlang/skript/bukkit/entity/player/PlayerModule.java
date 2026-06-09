cf609feeefa9e7e58119988d7ca558482e561ac6
AnOwlBe <iamaowlbe@gmail.com>
2026-06-08 22:15:16 -0400

    Statistics!

--- src/main/java/org/skriptlang/skript/bukkit/entity/player/PlayerModule.java
+++ src/main/java/org/skriptlang/skript/bukkit/entity/player/PlayerModule.java
@@ -1,14 +1,18 @@
 package org.skriptlang.skript.bukkit.entity.player;
 
 import ch.njol.skript.Skript;
+import ch.njol.skript.classes.EnumClassInfo;
 import ch.njol.skript.lang.util.SimpleEvent;
+import ch.njol.skript.registrations.Classes;
 import io.papermc.paper.event.player.AsyncChatEvent;
+import org.bukkit.Statistic;
 import org.skriptlang.skript.addon.AddonModule;
 import org.skriptlang.skript.addon.HierarchicalAddonModule;
 import org.skriptlang.skript.addon.SkriptAddon;
 import org.skriptlang.skript.bukkit.entity.player.elements.effects.*;
 import org.skriptlang.skript.bukkit.entity.player.elements.events.*;
 import org.skriptlang.skript.bukkit.entity.player.elements.expressions.*;
+import org.skriptlang.skript.bukkit.lang.eventvalue.EventValueRegistry;
 import org.skriptlang.skript.bukkit.registration.BukkitSyntaxInfos;
 import org.skriptlang.skript.registration.SyntaxRegistry;
 
@@ -16,6 +20,18 @@ public class PlayerModule extends HierarchicalAddonModule {
 
 	public PlayerModule(AddonModule parentModule) {
 		super(parentModule);
+	}
+
+	@Override
+	protected void initSelf(SkriptAddon addon) {
+		Classes.registerClass(new EnumClassInfo<>(Statistic.class, "statistic", "statistics")
+			.user("statistics?")
+			.name("Statistic")
+			.description(
+				"Represents a countable statistic, which is tracked by the server. "
+					+ "For the list of all available statistics, "
+					+ "see <a href='https://minecraft.wiki/w/Statistics'>the Minecraft wiki</a>.")
+			.since("INSERT VERSION"));
 	}
 
 	@Override
@@ -31,7 +47,8 @@ public class PlayerModule extends HierarchicalAddonModule {
 			ExprOnScreenKickMessage::register,
 			ExprPlayerListHeaderFooter::register,
 			ExprPlayerListName::register,
-			ExprQuitMessage::register
+			ExprQuitMessage::register,
+			ExprStatistic::register
 		);
 		if (Skript.classExists("io.papermc.paper.event.player.PlayerPickBlockEvent")) {
 			register(addon,
@@ -40,7 +57,11 @@ public class PlayerModule extends HierarchicalAddonModule {
 			);
 		}
 
+		EventValueRegistry eventValueRegistry = addon.registry(EventValueRegistry.class);
 		SyntaxRegistry syntaxRegistry = moduleRegistry(addon);
+
+		EvtPlayerStatisticChange.register(syntaxRegistry, eventValueRegistry);
+
 		syntaxRegistry.register(BukkitSyntaxInfos.Event.KEY, BukkitSyntaxInfos.Event.builder(SimpleEvent.class, "Chat")
 			.addDescription("Called whenever a player chats.",
 				"Use <a href='#ExprChatFormat'>chat format</a> to change message format.",
---
+++ src/main/java/org/skriptlang/skript/bukkit/entity/player/elements/events/EvtPlayerStatisticChange.java
@@ -0,0 +1,99 @@
+package org.skriptlang.skript.bukkit.entity.player.elements.events;
+
+import ch.njol.skript.lang.Literal;
+import ch.njol.skript.lang.SkriptEvent;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.SyntaxStringBuilder;
+import org.bukkit.Material;
+import org.bukkit.Statistic;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.Event;
+import org.bukkit.event.player.PlayerStatisticIncrementEvent;
+import org.jetbrains.annotations.Nullable;
+import org.skriptlang.skript.bukkit.lang.eventvalue.EventValue;
+import org.skriptlang.skript.bukkit.lang.eventvalue.EventValueRegistry;
+import org.skriptlang.skript.bukkit.registration.BukkitSyntaxInfos;
+import org.skriptlang.skript.registration.SyntaxRegistry;
+
+@SuppressWarnings("unchecked")
+
+public class EvtPlayerStatisticChange extends SkriptEvent {
+
+	public static void register(SyntaxRegistry syntaxRegistry, EventValueRegistry registry) {
+		syntaxRegistry.register(BukkitSyntaxInfos.Event.KEY, BukkitSyntaxInfos.Event.builder(EvtPlayerStatisticChange.class, "Player Statistic Change")
+			.supplier(EvtPlayerStatisticChange::new)
+			.addEvent(PlayerStatisticIncrementEvent.class)
+			.addPatterns("player statistic (change|increase|increment) [of %-statistics% statistic[s]]")
+			.addDescription("""
+				Called when a player's statistic changes.
+				Some statistics like 'play one minute' do not call this event, because they get called too often.
+				This event is only called when the server updates a statistic and as such does not call if ExprStatistic is used.
+				""")
+			.addExample("""
+						on player statistic increase:
+							broadcast "%player%'s statistic '%event-statistic%' increased! It is now %event-number%!
+						
+						on player statistic increase of leave game:
+							broadcast "%player% has now left the game %event-number% times!"
+						""")
+			.addSince("INSERT VERSION")
+			.build());
+
+		registry.register(EventValue.builder(PlayerStatisticIncrementEvent.class, Integer.class)
+			.getter(PlayerStatisticIncrementEvent::getNewValue)
+			.patterns("number")
+			.build());
+
+		registry.register(EventValue.builder(PlayerStatisticIncrementEvent.class, Integer.class)
+			.getter(PlayerStatisticIncrementEvent::getPreviousValue)
+			.patterns("number")
+			.time(EventValue.Time.PAST)
+			.build());
+
+		registry.register(EventValue.builder(PlayerStatisticIncrementEvent.class, Statistic.class)
+			.getter(PlayerStatisticIncrementEvent::getStatistic)
+			.patterns("statistic")
+			.build());
+
+		registry.register(EventValue.builder(PlayerStatisticIncrementEvent.class, Material.class)
+			.getter(PlayerStatisticIncrementEvent::getMaterial)
+			.patterns("material")
+			.build());
+
+		registry.register(EventValue.builder(PlayerStatisticIncrementEvent.class, EntityType.class)
+			.getter(PlayerStatisticIncrementEvent::getEntityType)
+			.patterns("entity data")
+			.build());
+
+	}
+
+	private Literal<Statistic> statistics;
+
+	@Override
+	public boolean init(Literal<?>[] literals, int i, SkriptParser.ParseResult parseResult) {
+		statistics = (Literal<Statistic>) literals[0];
+		return true;
+	}
+
+	@Override
+	public boolean check(Event event) {
+		if (statistics != null) {
+			Statistic statistic = ((PlayerStatisticIncrementEvent) event).getStatistic();
+			for (Statistic value : this.statistics.getAll(event)) {
+				if (statistic.equals(value))
+					return true;
+			}
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString(@Nullable Event event, boolean debug) {
+		SyntaxStringBuilder builder = new SyntaxStringBuilder(event, debug);
+		builder.append("player statistic change");
+		builder.appendIf(statistics != null, "of " + statistics.toString(event, debug));
+		return builder.toString();
+	}
+
+}
